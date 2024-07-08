4398a76136590b393fa09525fbb762bff7c08b17
HUIJAEKO <xrhgmlwox@naver.com>
2024-07-08 20:31:50 +0900

    mongosh

--- src/main/java/org/example/chat/ChatController.java
+++ src/main/java/org/example/chat/ChatController.java
@@ -1,6 +1,7 @@
 package org.example.chat;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
