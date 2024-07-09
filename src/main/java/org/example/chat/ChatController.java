2dafa5b272ad25c0b992edca83460c4c2c83f896
HUIJAEKO <xrhgmlwox@naver.com>
2024-07-09 14:01:34 +0900

    vscode연동

--- src/main/java/org/example/chat/ChatController.java
+++ src/main/java/org/example/chat/ChatController.java
@@ -16,12 +16,14 @@ public class ChatController {
 
     private final ChatRepository chatRepository;
 
+    @CrossOrigin
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver){
         return chatRepository.mFindBySender(sender, receiver)
             .subscribeOn(Schedulers.boundedElastic());
     }
 
+    @CrossOrigin
     @PostMapping("/chat")
     public Mono<Chat> setMsg(@RequestBody Chat chat){
         chat.setCreatedAt(LocalDateTime.now());
